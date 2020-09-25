ffbc8e69773718a61d2c605c29d0b776aef6e550
Your Name <you@example.com>
2020-09-25 22:35:53 +0600

    done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCts4X4qstutcDeD5hzzl5_VhpXWcQZSn4",
+    authDomain: "travel-guru-a4604.firebaseapp.com",
+    databaseURL: "https://travel-guru-a4604.firebaseio.com",
+    projectId: "travel-guru-a4604",
+    storageBucket: "travel-guru-a4604.appspot.com",
+    messagingSenderId: "484154089723",
+    appId: "1:484154089723:web:8ee07aec1899079255905e"
+};
+export default firebaseConfig;
